d38331cd3e8a7ce33040ab4de651f6f71afe706b
dmitry <dmitry@example.com>
2026-03-07 18:48:41 +0300

    тест токенов

--- src/infrastructure/auth/auth_service.py
+++ src/infrastructure/auth/auth_service.py
@@ -23,26 +23,26 @@ class AuthService:
             padding = "=" * (-len(data) % 4)
             return base64.urlsafe_b64decode(data + padding)
 
-        # try:
-        header_b64, _, *_ = token.split(".")
-        print(token)
-        header = json.loads(b64url_decode(header_b64))
+        try:
+            header_b64, _, *_ = token.split(".")
+            print(token)
+            header = json.loads(b64url_decode(header_b64))
 
-        kid = header["kid"]
-        key = await self.jwks.get_key(kid)
-        claims = self.settings.jwt.decode(
-            token,
-            key,
-            claims_options={
-                "iss": {"essential": True, "value": self.settings.issuer},
-                "sub": {"essential": True},
-                "exp": {"essential": True},
-                "iat": {"essential": True},
-            },
-        )
-        claims.validate()
+            kid = header["kid"]
+            key = await self.jwks.get_key(kid)
+            claims = self.settings.jwt.decode(
+                token,
+                key,
+                claims_options={
+                    "iss": {"essential": True, "value": self.settings.issuer},
+                    "sub": {"essential": True},
+                    "exp": {"essential": True},
+                    "iat": {"essential": True},
+                },
+            )
+            claims.validate()
 
-        return UUID(claims["sub"])
+            return UUID(claims["sub"])
 
-        # except (ExpiredTokenError, InvalidClaimError, JoseError, ValueError) as e:
-        #     raise InvalidTokenError(f"{e}") from e
+        except (ExpiredTokenError, InvalidClaimError, JoseError, ValueError) as e:
+            raise InvalidTokenError(f"{e}") from e
