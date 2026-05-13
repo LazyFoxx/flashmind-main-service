e8a0a1d0a16bce73626682952dc87f979d9d71c7
dmitry <dmitry@MacBook-Pro-dmitry.local>
2026-05-12 18:07:32 -0700

    подтянул прод

--- src/presentation/api/routers/v1/card.py
+++ src/presentation/api/routers/v1/card.py
@@ -15,6 +15,7 @@ from src.application.use_cases import (
     UpdateCardInput,
     UpdateCardUseCase,
 )
+
 from src.presentation.api.dependencies.auth import get_current_user_id
 from src.presentation.api.dto.v1 import (
     CardLightResponse,
